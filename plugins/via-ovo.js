ae505d4cd703b9a95daad645293a802cb7a400d9
Pino <109092712+hy-pin@users.noreply.github.com>
2022-07-22 03:13:50 +0700

    Update via-ovo.js

--- plugins/via-ovo.js
+++ plugins/via-ovo.js
@@ -5,16 +5,16 @@ Hay👋‹, ingin Donasi?
 
 💌 *Pembayaran*
 💳 Via: Ovo
-📞 Nomor: 088215689772
-👤 A/n: Fangz
-📝 Mitra: Fangz BOT
+📞 Nomor: -
+👤 A/n: -
+📝 Mitra: Pino BOT
 💰 Metode pembayaran: Online ( ~Cod~ )
 
 
 ®KLIK *SUDAH BAYAR* JIKA SUDAH TRANSFER 
 `
 const templateButtons = [
-    {index: 1, urlButton: {displayText: 'QRIS', url: 'https://telegra.ph/file/ff71853efeb006d635a90.jpg'}},
+    {index: 1, urlButton: {displayText: 'JOIN', url: 'https://chat.whatsapp.com/CmlrDN2xl0lCl4eBHLe6IJ'}},
     {index: 4, quickReplyButton: {displayText: 'Sudah membayar', id: '.sudahbayar'}},
 ]
 let tm = {
