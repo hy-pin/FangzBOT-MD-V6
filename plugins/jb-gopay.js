007a7281beed3649b1e0da4e86b2efe22f5d3780
Pino <109092712+hy-pin@users.noreply.github.com>
2022-07-22 03:00:52 +0700

    Update jb-gopay.js

--- plugins/jb-gopay.js
+++ plugins/jb-gopay.js
@@ -5,18 +5,12 @@ Hay👋‹, ingin melanjutkan pembayaran?
 
 💌 *Pembayaran*
 💳 Via: Gopay
-📞 Nomor: 088215689772
-👤 A/n: Fangz
-📦 Mitra: Fangz BOT
+📞 Nomor: 08895377665
+👤 A/n: Pino MD
+📦 Mitra: Pino BOT
 💰 Metode pembayaran: Online ( ~Cod~ )
 
 📣 *Beli*
-
-Apk TokoBot: https://app.bukaolshop.com/toko/tokobot
-
-Apk FangzApp:https://web.jagel.id/store/fangzapp
-
-Toko Website:https://toko.ly/TokoBot   (Non apk)
 
 ✏️ _Informasi Pembayaran_
 
@@ -27,7 +21,7 @@ Pastikan saldo kamu mencukupi untuk bertransaksi!
 ❗KLIK *SUDAH BAYAR* JIKA SUDAH MEMBAYAR!
 `
 const templateButtons = [
-    {index: 1, urlButton: {displayText: 'QRIS', url: 'https://telegra.ph/file/ff71853efeb006d635a90.jpg'}},
+    {index: 1, urlButton: {displayText: 'QRIS', url: 'https://telegra.ph/file/43828a2b037e6bf438c16.jpg'}},
     {index: 4, quickReplyButton: {displayText: 'Sudah membayar', id: '.sudahbayar'}},
 ]
 let tm = {
